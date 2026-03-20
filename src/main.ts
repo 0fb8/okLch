eff56c64441d3dd1071cf9ab367dda95291fa470
0fb8 <218385633+0fb8@users.noreply.github.com>
2026-03-20 12:53:45 +0900

    refactor: switch to oklch syntax in updateColor()

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ const colorValue = document.getElementById("colorValue") as HTMLSpanElement;
 const previewBox = document.getElementById("preview") as HTMLDivElement;
 
 const updateColor = (color: string): void => {
+  const oklchText = `oklch(from ${color} l c h)`;
+  console.log(oklchText);
   colorValue.textContent = color;
-  previewBox.style.backgroundColor = color;
+  previewBox.style.backgroundColor = oklchText;
 };
 
 updateColor(colorInput.value);
