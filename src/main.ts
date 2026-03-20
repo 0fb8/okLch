8f6d3704160fafd5261f025e075b5a9ad62435f0
0fb8 <218385633+0fb8@users.noreply.github.com>
2026-03-20 12:55:06 +0900

    add: updatePalette()

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,21 @@ const updateColor = (color: string): void => {
   previewBox.style.backgroundColor = oklchText;
 };
 
+const updatePalette = (color: string) => {
+  const steps = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+  palette.innerHTML = "";
+
+  steps.forEach((l) => {
+    const oklchText = `oklch(from ${color} ${l}% c h)`;
+    console.log(oklchText);
+
+    const swatch = document.createElement("div");
+    swatch.style.backgroundColor = oklchText;
+    swatch.innerHTML = `<span>L: ${l}%</span>`;
+    palette.appendChild(swatch);
+  });
+};
+
 updateColor(colorInput.value);
 
 colorInput.addEventListener("input", (event: Event) => {
